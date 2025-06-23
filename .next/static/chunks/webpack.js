fb94e4034765c17cf77a30fdefe8b72c941065bd
Simon Gneuß <93795566+Simonstorms@users.noreply.github.com>
2025-06-22 21:41:18 -0700

    fix folder strcucter

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9aa5b7694c444b07"; }
+/******/ 		__webpack_require__.h = function() { return "d9c8451c2a7f07ea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
